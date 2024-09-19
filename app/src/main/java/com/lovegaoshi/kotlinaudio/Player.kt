7007ffb4e2765472846ddbc124c37ec08cd36d54
lovegaoshi <106490582+lovegaoshi@users.noreply.github.com>
2024-09-19 14:23:52 -0700

    feat: init

--- app/src/main/java/com/lovegaoshi/kotlinaudio/Player.kt
+++ app/src/main/java/com/lovegaoshi/kotlinaudio/Player.kt
@@ -5,14 +5,24 @@ import androidx.annotation.OptIn
 import androidx.media3.common.AudioAttributes
 import androidx.media3.common.C
 import androidx.media3.common.ForwardingPlayer
+import androidx.media3.common.Player
 import androidx.media3.common.util.UnstableApi
 import androidx.media3.database.DatabaseProvider
 import androidx.media3.database.StandaloneDatabaseProvider
 import androidx.media3.datasource.cache.LeastRecentlyUsedCacheEvictor
 import androidx.media3.datasource.cache.SimpleCache
+import androidx.media3.exoplayer.DefaultLoadControl
+import androidx.media3.exoplayer.DefaultLoadControl.Builder
+import androidx.media3.exoplayer.DefaultLoadControl.DEFAULT_BACK_BUFFER_DURATION_MS
+import androidx.media3.exoplayer.DefaultLoadControl.DEFAULT_BUFFER_FOR_PLAYBACK_AFTER_REBUFFER_MS
+import androidx.media3.exoplayer.DefaultLoadControl.DEFAULT_BUFFER_FOR_PLAYBACK_MS
+import androidx.media3.exoplayer.DefaultLoadControl.DEFAULT_MAX_BUFFER_MS
+import androidx.media3.exoplayer.DefaultLoadControl.DEFAULT_MIN_BUFFER_MS
 import androidx.media3.exoplayer.ExoPlayer
+import com.lovegaoshi.kotlinaudio.models.BufferOptions
 import com.lovegaoshi.kotlinaudio.models.PlayerOptions
 import com.lovegaoshi.kotlinaudio.models.setContentType
+import com.lovegaoshi.kotlinaudio.models.setWakeMode
 import java.io.File
 
 class Player {
@@ -32,7 +42,12 @@ class Player {
         }
         exoPlayer = ExoPlayer
             .Builder(context)
+            .setHandleAudioBecomingNoisy(options.handleAudioBecomingNoisy)
             .setMediaSourceFactory(MediaFactory(context, cache))
+            .setWakeMode(setWakeMode(options.wakeMode))
+            .apply {
+                setLoadControl(setupBuffer(options.bufferOptions))
+            }
             .build()
 
         val audioAttributes = AudioAttributes.Builder()
@@ -42,6 +57,29 @@ class Player {
         exoPlayer.setAudioAttributes(audioAttributes, options.handleAudioFocus);
 
         player = object : ForwardingPlayer(exoPlayer) {
+
+
+            override fun isCommandAvailable(command: Int): Boolean {
+                if (options.alwaysShowNext) {
+                    return when (command) {
+                        COMMAND_SEEK_TO_NEXT_MEDIA_ITEM -> true
+                        COMMAND_SEEK_TO_NEXT -> true
+                        else -> super.isCommandAvailable(command)
+                    }
+                }
+                return super.isCommandAvailable(command)
+            }
+
+            override fun getAvailableCommands(): Player.Commands {
+                if (options.alwaysShowNext) {
+                    return super.getAvailableCommands().buildUpon()
+                        .add(COMMAND_SEEK_TO_NEXT_MEDIA_ITEM)
+                        .add(COMMAND_SEEK_TO_NEXT)
+                        .build()
+                }
+                return super.getAvailableCommands()
+            }
+
             override fun play() {
                 // Add custom logic
                 super.play()
@@ -53,4 +91,24 @@ class Player {
         }
     }
 
+}
+
+private fun setupBuffer(bufferConfig: BufferOptions): DefaultLoadControl {
+    bufferConfig.apply {
+        val multiplier =
+            DEFAULT_BUFFER_FOR_PLAYBACK_AFTER_REBUFFER_MS / DEFAULT_BUFFER_FOR_PLAYBACK_MS
+        val minBuffer =
+            if (minBuffer != null && minBuffer != 0) minBuffer else DEFAULT_MIN_BUFFER_MS
+        val maxBuffer =
+            if (maxBuffer != null && maxBuffer != 0) maxBuffer else DEFAULT_MAX_BUFFER_MS
+        val playBuffer =
+            if (playBuffer != null && playBuffer != 0) playBuffer else DEFAULT_BUFFER_FOR_PLAYBACK_MS
+        val backBuffer =
+            if (backBuffer != null && backBuffer != 0) backBuffer else DEFAULT_BACK_BUFFER_DURATION_MS
+
+        return Builder()
+            .setBufferDurationsMs(minBuffer, maxBuffer, playBuffer, playBuffer * multiplier)
+            .setBackBuffer(backBuffer, false)
+            .build()
+    }
 }
--- app/src/main/java/com/lovegaoshi/kotlinaudio/models/PlayerOptions.kt
+++ app/src/main/java/com/lovegaoshi/kotlinaudio/models/PlayerOptions.kt
@@ -1,11 +1,27 @@
+@file: OptIn(UnstableApi::class)
 package com.lovegaoshi.kotlinaudio.models
 
+import androidx.annotation.OptIn
 import androidx.media3.common.C
+import androidx.media3.common.util.UnstableApi
 
 data class PlayerOptions(
     val cacheSize: Long = 0,
     val audioContentType: Int = 0,
-    val handleAudioFocus: Boolean = true
+    val wakeMode: Int = 0,
+    val handleAudioBecomingNoisy: Boolean = false,
+    val alwaysShowNext: Boolean = true,
+    val handleAudioFocus: Boolean = true,
+    val bufferOptions: BufferOptions = BufferOptions(null, null, null, null)
+)
+
+data class BufferOptions (
+
+    val minBuffer: Int?,
+    val maxBuffer: Int?,
+    val playBuffer: Int?,
+    val backBuffer: Int?,
+
 )
 
 fun setContentType (type: Int = 0): Int {
@@ -15,5 +31,13 @@ fun setContentType (type: Int = 0): Int {
         3 -> C.AUDIO_CONTENT_TYPE_MOVIE
         4 -> C.AUDIO_CONTENT_TYPE_UNKNOWN
         else -> C.AUDIO_CONTENT_TYPE_MUSIC
+    }
+}
+
+fun setWakeMode(type: Int = 0): Int {
+    return when (type) {
+        1 -> C.WAKE_MODE_LOCAL
+        2 -> C.WAKE_MODE_NETWORK
+        else -> C.WAKE_MODE_NONE
     }
 }
