d20d01a5f350d2984a3693a4d8d1aa0a6186b2b9
lovegaoshi <106490582+lovegaoshi@users.noreply.github.com>
2024-09-19 13:37:47 -0700

    feat: cache

--- app/src/main/java/com/lovegaoshi/kotlinaudio/Player.kt
+++ app/src/main/java/com/lovegaoshi/kotlinaudio/Player.kt
@@ -4,17 +4,32 @@ import android.content.Context
 import androidx.annotation.OptIn
 import androidx.media3.common.ForwardingPlayer
 import androidx.media3.common.util.UnstableApi
+import androidx.media3.database.DatabaseProvider
+import androidx.media3.database.StandaloneDatabaseProvider
+import androidx.media3.datasource.cache.LeastRecentlyUsedCacheEvictor
+import androidx.media3.datasource.cache.SimpleCache
 import androidx.media3.exoplayer.ExoPlayer
+import com.lovegaoshi.kotlinaudio.models.PlayerOptions
+import java.io.File
 
 class Player {
     lateinit var exoPlayer: ExoPlayer
     lateinit var player: ForwardingPlayer
+    private var cache: SimpleCache? = null
 
-    fun setupPlayer(context: Context) {
+    fun setupPlayer(context: Context, options: PlayerOptions = PlayerOptions()) {
 
+        if (options.cacheSize > 0) {
+            val db: DatabaseProvider = StandaloneDatabaseProvider(context)
+            cache = SimpleCache(
+                File(context.cacheDir, "APM"),
+                LeastRecentlyUsedCacheEvictor(options.cacheSize),
+                db
+            )
+        }
         exoPlayer = ExoPlayer
             .Builder(context)
-            .setMediaSourceFactory(MediaFactory(context, cache = null))
+            .setMediaSourceFactory(MediaFactory(context, cache))
             .build()
         player = object : ForwardingPlayer(exoPlayer) {
             override fun play() {
---
+++ app/src/main/java/com/lovegaoshi/kotlinaudio/models/PlayerOptions.kt
@@ -0,0 +1,5 @@
+package com.lovegaoshi.kotlinaudio.models
+
+data class PlayerOptions (
+    val cacheSize: Long = 0
+)
